f64fcbfb7411d983b05548e84f0d84cf01c076ce
Dustin Westaby <thatdecade@gmail.com>
2024-05-30 12:51:46 -0500

    Created configuration module

--- Source/code.py
+++ Source/code.py
@@ -11,133 +11,56 @@ import board
 import asyncio
 from adafruit_bitmap_font import bitmap_font
 from user_requests import UserRequest
+import configure_me as config
 
-# -----------------------------------------------------------------------------
-# Board Configuration
-# -----------------------------------------------------------------------------
-
-# Board setup imports (uncomment the one corresponding to your board)
-import board_lilygo as board_setup            # LilyGo RP2040 w/ 1.14" LCD    https://amzn.to/4aBkma1
-# import board_esp32_s3 as board_setup          # Adafruit ESP32-S3 Feather     https://www.adafruit.com/product/5477
-# import board_feather_m4 as board_setup        # Adafruit Feather M4 Express   https://www.adafruit.com/product/3857
-# import board_itsybitsy_m4 as board_setup      # Adafruit ItsyBitsy M4 Express https://www.adafruit.com/product/3800
-# import board_itsybitsy_rp2040 as board_setup  # Adafruit ItsyBitsy RP2040     https://www.adafruit.com/product/4888
-# import board_pipico_rp2040 as board_setup     # Raspberry Pi Pico RP2040      https://www.adafruit.com/product/4864
-
-# -----------------------------------------------------------------------------
-# Display Configuration
-# -----------------------------------------------------------------------------
-
-# Display type
-# Choose the type of display you are using. Only one should be uncommented.
-DISPLAY = "1.14" #Pre-Beskar   # Adafruit 1.14" LCD display  https://www.adafruit.com/product/4383
-# DISPLAY = "1.30" #"Beskar"     # Adafruit 1.3" LCD display   https://www.adafruit.com/product/4313
-# DISPLAY = "1.44"               # Adafruit 1.44" LCD display  https://www.adafruit.com/product/2088
-# DISPLAY = "0.96"               # Adafruit 0.96" LCD display  https://www.adafruit.com/product/3533
-
-# Backlight brightness (0% to 100%)
-DISP_BRIGHT = 80
-
-# Owner name settings
-SHOW_NAME = 0                     # Set to 1 to display the name, or 0 to not display a name
-OWNER_NAME = "Your Name Here"     # Name of the owner to be shown
-NAME_COLOR = 0x00FF00             # Green on black (choose colors here - https://www.color-hex.com/)
-NAME_HOLD = 3.0                   # How many seconds to display the name
-
-# Battery monitoring settings
-BATTERY_SZ = 500  # Size of battery in mAh (only for the ESP32-S3 board)
-BATTERY_MON = 1  # Set to 1 to enable the battery monitor, 0 to disable it
-LOW_BATT_LEVEL = 20  # Show the low battery icon when the battery goes below this percentage
-LOW_BATT_SHUTDOWN_LEVEL = 10  # Shutdown when battery goes below this percentage
-
-# Other settings for debugging
-ENABLE_LEDS = 1  # Set to 1 to turn on LEDs for debugging, set to 0 to save battery
-DEBUG_SERIAL = 1  # If your board has a debug port, you can read the system messages.
-
-SLEEP_TIMEOUT_SECONDS = 3600 # 3600 seconds = 1 hour
-
-# -----------------------------------------------------------------------------
-# Banner graphics settings
-# -----------------------------------------------------------------------------
-
-# Setting screen resolution based on display type, do not change these
-if DISPLAY == "1.30":
-    RESOLUTION = 240
-elif DISPLAY == "1.14":
-    RESOLUTION = 135
-elif DISPLAY == "1.44":
-    RESOLUTION = 128
-elif DISPLAY == "0.96":
-    RESOLUTION = 80
-
-# Banner graphic(s) shown after the owner's name and before the sequence starts
-SHOW_IMG = 0  # How many images to show. 0 = no images, 1 = 1 image, 2 = 2 images
-
-IMG1 = f"TheMandalorian{RESOLUTION}.bmp"  # File name of the first 8-bit BMP graphic to be shown after each text sequence
-IMG1_HOLD = 5.00  # How long the first image is displayed in seconds
-
-IMG2 = f"BabyYoda{RESOLUTION}.bmp"  # File name of the second 8-bit BMP graphic to be shown after the first image
-IMG2_HOLD = 5.00  # How long the second image is displayed in seconds
-
-# -----------------------------------------------------------------------------
-# Mandalorian character sequence
-# -----------------------------------------------------------------------------
-
-messages = ["MLM", "JBM", "SAS", "JAS", "JBM", "MLM", "SAS", "AJS", "SAS"]
-# Time that each character group is shown 0.50 is 500 milliseconds, or 1/2 of a second
-delays = [0.75, 0.75, 0.65, 0.75, 0.50, 0.84, 1.00, 0.35, 0.84]
-TEXT_COLOR = 0xFF0000  # Red on black (choose colors here - https://www.color-hex.com/)
-
-# -----------------------------------------------------------------------------
-# DO NOT EDIT BELOW THIS LINE. THESE ARE THE MAIN FUNCTIONS
 # -----------------------------------------------------------------------------
 
 def debug_print(text):
-    if DEBUG_SERIAL:
+    if config.DEBUG_SERIAL:
         print(text)
 
-debug_print(f"Starting MandoPuter in {DISPLAY} mode.")
+debug_print(f"Starting MandoPuter in {config.DISPLAY} mode.")
 
 # Setup
-display_bus, lcd_light = board_setup.init()
+display_bus, lcd_light = config.board_setup.init()
 
 debug_print("Display bus initialized")
 
 # Initialize display
-display, font, offset = display_images.init(DISPLAY, display_bus, lcd_light)
+display, font, offset = display_images.init(config.DISPLAY, display_bus, lcd_light)
 debug_print("Display initialized")
 
-led = board_setup.configure_led(ENABLE_LEDS)
+led = config.board_setup.configure_led(config.ENABLE_LEDS)
 debug_print("LED configured")
 
-if BATTERY_MON:
-    battery_monitoring = board_setup.configure_battery_monitor()
+if config.BATTERY_MON:
+    battery_monitoring = config.board_setup.configure_battery_monitor()
     debug_print("Battery monitor configured")
 
 # Turn on the Backlight
-display.brightness = DISP_BRIGHT / 100
+display.brightness = config.DISP_BRIGHT / 100
 debug_print("Backlight turned on")
 
 # Show the owner's name at startup
-if SHOW_NAME:
-    display_images.display_name(display, font, OWNER_NAME, NAME_HOLD, NAME_COLOR)
-    debug_print(f"Displayed owner name: {OWNER_NAME}")
+if config.SHOW_NAME:
+    display_images.display_name(display, font, config.OWNER_NAME, config.NAME_HOLD, config.NAME_COLOR)
+    debug_print(f"Displayed owner name: {config.OWNER_NAME}")
 
 # Show the banner graphic(s)
-if SHOW_IMG:
-    debug_print(f"Display image: {IMG1}")
-    display_images.display_image(display, IMG1, IMG1_HOLD)
-    if SHOW_IMG > 1:
-        debug_print(f"Display image: {IMG2}")
-        display_images.display_image(display, IMG2, IMG2_HOLD)
+if config.SHOW_IMG:
+    debug_print(f"Display image: {config.IMG1}")
+    display_images.display_image(display, config.IMG1, config.IMG1_HOLD)
+    if config.SHOW_IMG > 1:
+        debug_print(f"Display image: {config.IMG2}")
+        display_images.display_image(display, config.IMG2, config.IMG2_HOLD)
 
 button_pins = None
 buttons = None
-if hasattr(board_setup, 'configure_buttons'):
-    button_pins = board_setup.configure_buttons()
+if hasattr(config.board_setup, 'configure_buttons'):
+    button_pins = config.board_setup.configure_buttons()
     
     # Initialize sleep module
-    sleep.init(button_pins, SLEEP_TIMEOUT_SECONDS)
+    sleep.init(button_pins, config.SLEEP_TIMEOUT_SECONDS)
     debug_print("Sleep timer started")
 
     # Setup buttons for user interaction
@@ -158,13 +81,13 @@ async def battery_monitoring_routine(stage, display, battery_monitoring, user_re
         batt_percent = battery_monitor.get_batt_percent(battery_monitoring)
         debug_print(f"Battery percentage: {batt_percent}%")
         
-        if batt_percent < LOW_BATT_SHUTDOWN_LEVEL:
+        if batt_percent < config.LOW_BATT_SHUTDOWN_LEVEL:
             debug_print("Entering deep sleep due to low battery")
             display_images.display_name(display, bitmap_font.load_font("Alef-Bold-12.bdf"), "Battery Low.\nShutting Down...", 5, 0xFF0000)
             user_request.stop_polling()
             sleep.deepsleep(button_pins)
         
-        elif batt_percent < LOW_BATT_LEVEL:
+        elif batt_percent < config.LOW_BATT_LEVEL:
             if low_batt_icon == 0:
                 stage.append(batt_tile)
                 low_batt_icon = 1
@@ -184,10 +107,10 @@ async def main():
 
     stage = displayio.Group()
     display.show(stage)
-    mando_message = label.Label(font, text=messages[0], color=TEXT_COLOR)
+    mando_message = label.Label(font, text=config.messages[0], color=config.TEXT_COLOR)
     stage.append(mando_message)
 
-    maxwidth = max(mando_message.bounding_box[2] for msg in messages)
+    maxwidth = max(mando_message.bounding_box[2] for msg in config.messages)
     mando_message.x = int((display.width - maxwidth) / 2) - 1
     mando_message.y = int(display.height / 2) + offset
 
@@ -198,11 +121,11 @@ async def main():
     stage.append(speed_indicator)
     speed_indicator.hidden = True
 
-    if BATTERY_MON:
+    if config.BATTERY_MON:
         asyncio.create_task(battery_monitoring_routine(stage, display, battery_monitoring, user_request))
 
     while True:
-        for index, msg in enumerate(messages):
+        for index, msg in enumerate(config.messages):
             mando_message.text = msg
             if user_request.get_scroll_speed_for_animation() > 0:
                 mando_message.x = display.width  # Reset the text position to the start for each message
@@ -226,7 +149,7 @@ async def main():
                 # Non-Moving Text
                 mando_message.x = int((display.width - maxwidth) / 2) - 1
                 display.refresh()
-                await asyncio.sleep(delays[index])
+                await asyncio.sleep(config.delays[index])
                 
                 # Check for user requested changes
                 if user_request.should_show_speed_indicator():
---
+++ Source/configure_me.py
@@ -0,0 +1,73 @@
+# configure_me.py
+
+# -----------------------------------------------------------------------------
+# Board Configuration
+# -----------------------------------------------------------------------------
+
+# Board setup imports (uncomment the one corresponding to your board)
+import board_lilygo as board_setup            # LilyGo RP2040 w/ 1.14" LCD    https://amzn.to/4aBkma1
+# import board_esp32_s3 as board_setup          # Adafruit ESP32-S3 Feather     https://www.adafruit.com/product/5477
+# import board_feather_m4 as board_setup        # Adafruit Feather M4 Express   https://www.adafruit.com/product/3857
+# import board_itsybitsy_m4 as board_setup      # Adafruit ItsyBitsy M4 Express https://www.adafruit.com/product/3800
+# import board_itsybitsy_rp2040 as board_setup  # Adafruit ItsyBitsy RP2040     https://www.adafruit.com/product/4888
+# import board_pipico_rp2040 as board_setup     # Raspberry Pi Pico RP2040      https://www.adafruit.com/product/4864
+
+# -----------------------------------------------------------------------------
+# Display Configuration
+# -----------------------------------------------------------------------------
+
+# Display type
+DISPLAY = "1.14"  # Pre-Beskar   # Adafruit 1.14" LCD display  https://www.adafruit.com/product/4383
+# DISPLAY = "1.30" #"Beskar"     # Adafruit 1.3" LCD display   https://www.adafruit.com/product/4313
+# DISPLAY = "1.44"               # Adafruit 1.44" LCD display  https://www.adafruit.com/product/2088
+# DISPLAY = "0.96"               # Adafruit 0.96" LCD display  https://www.adafruit.com/product/3533
+
+# -----------------------------------------------------------------------------
+# Preference Configuration
+# -----------------------------------------------------------------------------
+
+# Backlight brightness (0% to 100%)
+DISP_BRIGHT = 80
+
+# Owner name settings
+SHOW_NAME = 0  # Set to 1 to display the name, or 0 to not display a name
+OWNER_NAME = "Your Name Here"  # Name of the owner to be shown
+NAME_COLOR = 0x00FF00  # Green on black (choose colors here - https://www.color-hex.com/)
+NAME_HOLD = 3.0  # How many seconds to display the name
+
+# Battery monitoring settings
+BATTERY_SZ = 500  # Size of battery in mAh (only for the ESP32-S3 board)
+BATTERY_MON = 1  # Set to 1 to enable the battery monitor, 0 to disable it
+LOW_BATT_LEVEL = 20  # Show the low battery icon when the battery goes below this percentage
+LOW_BATT_SHUTDOWN_LEVEL = 10  # Shutdown when battery goes below this percentage
+
+# Other settings for debugging
+ENABLE_LEDS = 1  # Set to 1 to turn on LEDs for debugging, set to 0 to save battery
+DEBUG_SERIAL = 1  # If your board has a debug port, you can read the system messages.
+
+SLEEP_TIMEOUT_SECONDS = 3600  # 3600 seconds = 1 hour
+
+# -----------------------------------------------------------------------------
+# Banner graphics settings
+# -----------------------------------------------------------------------------
+
+# Banner graphics settings
+SHOW_IMG = 0  # How many images to show. 0 = no images, 1 = 1 image, 2 = 2 images
+
+# File name of the first 8-bit BMP graphic to be shown after each text sequence
+IMG1 = f"TheMandalorian{{RESOLUTION}}.bmp"
+IMG1_HOLD = 5.00  # How long the first image is displayed in seconds
+
+# File name of the second 8-bit BMP graphic to be shown after the first image
+IMG2 = f"BabyYoda{{RESOLUTION}}.bmp"
+IMG2_HOLD = 5.00  # How long the second image is displayed in seconds
+
+# -----------------------------------------------------------------------------
+# Mandalorian character sequence
+# -----------------------------------------------------------------------------
+
+# Mandalorian character sequence
+messages = ["MLM", "JBM", "SAS", "JAS", "JBM", "MLM", "SAS", "AJS", "SAS"]
+# Time that each character group is shown 0.50 is 500 milliseconds, or 1/2 of a second
+delays = [0.75, 0.75, 0.65, 0.75, 0.50, 0.84, 1.00, 0.35, 0.84]
+TEXT_COLOR = 0xFF0000  # Red on black (choose colors here - https://www.color-hex.com/)
